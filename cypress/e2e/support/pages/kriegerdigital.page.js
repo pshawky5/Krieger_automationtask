af5d8d2470ca59fb0c24ee51d46cfd584f94c332
pshawky5 <pshawky5@gmail.com>
2024-04-26 14:57:54 +0200

    Add kriegerdigital.page.js to represent the Kriegerdigital webpage

---
+++ cypress/e2e/support/pages/kriegerdigital.page.js
@@ -0,0 +1,9 @@
+class KriegerdigitalPage {
+  get path() {
+    return 'https://www.Kriegerdigital.de';
+  }
+
+  // Define other page elements and methods here
+}
+
+module.exports = new KriegerdigitalPage();
